6da5940073f761861546d2a99e04500231ee98f6
Raghav Heda <raghav.heda@elastic.in>
2021-08-10 22:10:03 +0530

    Emoji Interpreter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
+
+  var emojiDictionary = {
+    "😊": "smiling",
+    "😉": "winking",
+    "😘": "kiss",
+    "😳": "disbelief",
+    "😔": "sad",
+    "🤔": "thinking",
+    "❤️": "love",
+    "😴": "sleeping",
+    "😑": "annoyance",
+    "😕": "confused",
+    "😠": "angry",
+    "🤓": "nerd"
+  };
+
+  var emojiList = Object.keys(emojiDictionary)
+  const [meaning,setMeaning]=useState("")
+
+  function inputChangeHandler(event){
+    var emoji = event.target.value
+    var meaning = emojiDictionary[emoji];
+
+    if (meaning === undefined) {
+      meaning = "Sorry, We don't have this in our database";
+    }
+    // ,output
+    setMeaning(meaning);
+  }
+
+  function knowEmoji(emoji){
+    setMeaning(emojiDictionary[emoji])
+  }
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Know Your Emojis</h1>
+      <input  placeholder="Enter the emoji to know it's emotions" onChange={inputChangeHandler}/>
+      <h2 style={{ color: "grey" }}> {meaning} </h2>
+      <h1>List Of Emojis</h1>
+      {emojiList.map(function (emoji) {
+        return (
+          <span
+            onClick={() => knowEmoji(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+
     </div>
-  );
+  )
 }
+
 
 export default App;
